1c26de594778fcd696ce590c295b7cd510832452
Abhinv001 <asinghofficial01@gmail.com>
2023-08-25 01:01:30 +0530

    Abhinav Portfolio

---
+++ script.js
@@ -0,0 +1,61 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+
+window.onscroll = () => {
+    sections.forEach(  sections =>{
+        let top = window.scrollY;
+        let offset = sections.offsetTop - 150;
+        let height = sections.offsetHeight;
+        let id = sections.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(navLinks => {
+                navLinks.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+
+    let header = document.querySelector('header');
+    
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}; 
+
+
+
+
+ScrollReveal({ 
+    // reset: true,
+    distance:'80px',
+    duration:2000,
+    delay: 200
+}); 
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .skill-container, .project-box, .contact form', { origin: 'bottom' });
+
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+var typed = new Typed('#ele',{
+    strings : ['Web Developer', 'Frontend Developer'],
+      typeSpeed: 100,
+      backSpeed:100,
+      backDelay:100,
+      loop:true
+});
